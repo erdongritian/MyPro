f3661261858c5109778aa40cf889f04ed056900e
hao_chen <erdongritian@foxmail.com>
2018-08-03 17:10:41 +0800

    commit leetcode

---
+++ src/com/htxa/ch/leetcode/Permutations.java
@@ -0,0 +1,25 @@
+package com.htxa.ch.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+
+    public static void main(String[] args) {
+        Permutations permutations=new Permutations();
+        permutations.permute(new int[]{1,2});
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+//        List<List<Integer>> result=new ArrayList<>();
+//
+//        result.add();
+//
+//        for(int i=0;i<nums.length;i++){
+//
+//
+//        }
+        return null;
+
+    }
+}
