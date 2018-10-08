447bc75b97a19f593347b96bab2a5e8235883957
hao_chen <erdongritian@foxmail.com>
2018-10-08 16:41:08 +0800

    commit leetcode

---
+++ src/com/htxa/ch/leetcode/NextPermutation.java
@@ -0,0 +1,9 @@
+package com.htxa.ch.leetcode;
+
+public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+
+    }
+
+}
--- src/com/htxa/ch/leetcode/PermutationsII.java
+++ src/com/htxa/ch/leetcode/PermutationsII.java
@@ -2,20 +2,27 @@ package com.htxa.ch.leetcode;
 
 import java.util.*;
 
-public class Permutations {
+public class PermutationsII {
 
     public static void main(String[] args) {
-        Permutations permutations = new Permutations();
-        int[] nums=new int[]{1,2,3,4};
-        List<List<Integer>> list=permutations.permute(nums);
+        PermutationsII permutations = new PermutationsII();
+        int[] nums=new int[]{1,2,1};
+        List<List<Integer>> list=permutations.permuteUnique(nums);
         System.out.println(list);
     }
 
-
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permuteUnique(int[] nums) {
         if(nums.length==1){
             return Arrays.asList(Arrays.asList(nums[0]));
         }
+
+        Arrays.sort(nums);
+        List<List<Integer>> result = getList(nums);
+
+        return result;
+    }
+
+    public List<List<Integer>> getList(int[] nums) {
 
         List<List<Integer>> result = new ArrayList<>();
         if(nums.length==2){
@@ -24,15 +31,20 @@ public class Permutations {
             l1.add(nums[1]);
             result.add(l1);
 
-            List<Integer> l2=new ArrayList<>();
-            l2.add(nums[1]);
-            l2.add(nums[0]);
-            result.add(l2);
-
+            if(nums[0]!=nums[1]){
+                List<Integer> l2=new ArrayList<>();
+                l2.add(nums[1]);
+                l2.add(nums[0]);
+                result.add(l2);
+            }
             return result;
         }else {
             int numsLength=nums.length;
             for (int i = 0; i < numsLength; i++) {
+                if(i>0&&nums[i]==nums[i-1]){
+                    continue;
+                }
+
                 int[] tempNums=new int[numsLength-1];
                 for (int j = 0; j < numsLength - 1; j++) {
                     if(j<i){
@@ -42,7 +54,7 @@ public class Permutations {
                     }
                 }
 
-                List<List<Integer>> tempList=permute(tempNums);
+                List<List<Integer>> tempList=permuteUnique(tempNums);
                 for (List<Integer> temp : tempList) {
                     temp.add(0,nums[i]);
                     result.add(temp);
@@ -52,12 +64,6 @@ public class Permutations {
 
         return result;
     }
-
-
-
-
-
-
 
 
 }
